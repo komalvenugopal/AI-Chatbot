64d3674f2de31aca3870af4b69911b70fc0d054c
Komalvenugopal <komal.vattumilli@jivox.com>
2022-08-04 20:45:33 +0530

    chat js

--- frontend/script/chat.js
+++ frontend/script/chat.js
@@ -11,17 +11,87 @@ function predict(message) {
 }
 
 var chatWindow = new Bubbles(document.getElementById("chat"), "chatWindow", {
+  // the one that we care about is inputCallbackFn()
+  // this function returns an object with some data that we can process from user input
+  // and understand the context of it
+
+  // this is an example function that matches the text user typed to one of the answer bubbles
+  // this function does no natural language processing
+  // this is where you may want to connect this script to NLC backend.
   inputCallbackFn: function(o) {
-    chatWindow.talk(
-      {
-        "i-dont-get-it": {
-          says: [
-            "Sorry I didn't get it, Please choose from below options"      
-          ],
-          reply: o.convo[o.standingAnswer].reply
-        }
-      }
-    )
+    // add error conversation block & recall it if no answer matched
+    var miss = function() {
+      chatWindow.talk(
+        {
+          "i-dont-get-it": {
+            says: [
+              "Sorry, I don't get it 😕. Pls repeat? Or you can just click below 👇"
+            ],
+            reply: o.convo[o.standingAnswer].reply
+          }
+        },
+        "i-dont-get-it"
+      )
+    }
+
+    // do this if answer found
+    var match = function(key) {
+      setTimeout(function() {
+        chatWindow.talk(convo, key) // restart current convo from point found in the answer
+      }, 600)
+    }
+
+    // sanitize text for search function
+    var strip = function(text) {
+      return text.toLowerCase().replace(/[\s.,\/#!$%\^&\*;:{}=\-_'"`~()]/g, "")
+    }
+
+    // search function
+    var found = false
+    console.log(o.convo[o.standingAnswer])
+    o.convo[o.standingAnswer].reply.forEach(function(e, i) {
+      strip(e.question).includes(strip(o.input)) && o.input.length > 0
+        ? (found = e.answer)
+        : found ? null : (found = false)
+    })
+    found ? match(found) : miss()
   }
-})
-chatWindow.talk(convo);
+}) // done setting up chat-bubble
+
+
+var convo = {
+  ice: {
+    says: ["Hi", "Would you like banana or ice cream?"],
+    reply: [
+      {
+        question: "Banana",
+        answer: "banana"
+      },
+      {
+        question: "Ice Cream",
+        answer: "ice-cream"
+      }
+    ]
+  },
+  banana: {
+    says: ["🍌"],
+    reply: [
+      {
+        question: "Start Over",
+        answer: "ice"
+      }
+    ]
+  },
+  "ice-cream": {
+    says: ["🍦"],
+    reply: [
+      {
+        question: "Start Over",
+        answer: "ice"
+      }
+    ]
+  }
+}
+
+// pass JSON to your function and you're done!
+chatWindow.talk(convo)
